48ef36cd815d8b23a2fe03e70a96dc5a54380bdc
Mohammed Al-Dahleh <maldahleh@gmail.com>
2017-11-19 15:02:20 -0500

    Base for Event Manager

---
+++ BoutTime/Events/EventManager.swift
@@ -0,0 +1,9 @@
+//
+//  EventManager.swift
+//  BoutTime
+//
+//  Created by Mohammed Al-Dahleh on 2017-11-19.
+//  Copyright © 2017 Mohammed Al-Dahleh. All rights reserved.
+//
+
+import Foundation
