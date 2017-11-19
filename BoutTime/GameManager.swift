76a5ce8ad71a7f18d25f0da55db837c0129da5c8
Mohammed Al-Dahleh <maldahleh@gmail.com>
2017-11-19 14:23:20 -0500

    Implement manager protocols

--- BoutTime/GameManager.swift
+++ BoutTime/GameManager.swift
@@ -9,15 +9,41 @@
 import Foundation
 
 protocol Manager {
-    var gameManager: GameScreen { get }
+    var viewManager: GameScreen { get }
 }
 
-class GameManager: Manager {
-    let gameManager: GameScreen
+protocol Playable {
+    var eventArray: [Event] { get set }
+}
+
+protocol Checkable {
+    func isMatched(checkArray: [Event]) -> Bool
+}
+
+protocol Resetable {
+    func newGame()
+}
+
+class GameManager: Manager, Playable, Checkable, Resetable {
+    let viewManager: GameScreen
+    var eventArray: [Event]
     
-    init (gameManager: GameScreen) {
-        self.gameManager = gameManager
+    init (viewManager: GameScreen) {
+        self.viewManager = viewManager
+        self.eventArray = []
     }
     
+    func newGame() {
+        
+    }
     
+    func isMatched(checkArray: [Event]) -> Bool {
+        for index in 0...eventArray.count {
+            if eventArray[index] != checkArray [index] {
+                return false
+            }
+        }
+        
+        return true
+    }
 }
