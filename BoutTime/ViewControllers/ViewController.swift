f606b0dc9ab282c12d98d97e43584b46b501a307
Mohammed Al-Dahleh <maldahleh@gmail.com>
2017-11-26 11:46:35 -0500

    Remove unnecessary fix comment

--- BoutTime/ViewControllers/ViewController.swift
+++ BoutTime/ViewControllers/ViewController.swift
@@ -8,8 +8,6 @@
 
 import UIKit
 
-// FIXME: rounding corners
-
 class ViewController: UIViewController {    
     @IBOutlet var eventButtons: [UIButton]!
     @IBOutlet var navigationButtons: [UIButton]!
